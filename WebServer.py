40f0a172e6be706b7618298011d5a0eecfb71419
nino <nino@nino.com>
2023-03-22 14:48:18 +0100

    Changes in application frontend

--- WebServer.py
+++ WebServer.py
@@ -9,15 +9,21 @@ GPIO.setwarnings(False)
 
 # define actuators GPIOs
 Relay = 40
+Sensor1 = 35
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(Relay, GPIO.OUT)
 GPIO.output(Relay, GPIO.LOW)
+#GPIO.setup(Sensor1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+#GPIO.setup(Sensor1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(Sensor1, GPIO.IN)
 
 @app.route("/")
 def index():
     RelaySts = GPIO.input(Relay)
+    Sensor1Sts = GPIO.input(Sensor1)
     templateData = {
         'Relay': RelaySts,
+        'Sensor1' : Sensor1Sts,
     }
     return render_template('index.html', **templateData)
 
@@ -32,5 +38,12 @@ def handle_relay_update(data):
     RelaySts = GPIO.input(Relay)
     emit('relay_status', {'Relay': RelaySts}, broadcast=True)
 
+def sensor1_callback(channel):
+    Sensor1Sts = GPIO.input(channel)
+    print("Sensor1Sts:", Sensor1Sts)  # Add this print statement
+    socketio.emit('sensor1_status', {'Sensor1': Sensor1Sts}, namespace='/')
+
+GPIO.add_event_detect(Sensor1, GPIO.BOTH, callback=sensor1_callback, bouncetime=50)
+
 if __name__ == "__main__":
-    socketio.run(app, host='0.0.0.0', port=80, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=80, debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,14 @@
-//connect to WebSocket server
 document.addEventListener('DOMContentLoaded', () => {
-  const socket = io.connect(); 
+  const socket = io.connect();
 
-  //Listens for 'relay_status' events and updates the DOM with the new status.
   socket.on('relay_status', (data) => {
     document.querySelector('#relay-status').textContent = data.Relay;
   });
 
-  //Defines a function sendUpdate for sending relay updates to the server.
   const sendUpdate = (action) => {
     socket.emit('relay_update', { action: action });
   };
-//Adds click event listeners for the "ON" and "OFF" buttons, calling the sendUpdate function with the appropriate action.
+
   document.querySelector('#relay-on').addEventListener('click', (e) => {
     e.preventDefault();
     sendUpdate('on');
