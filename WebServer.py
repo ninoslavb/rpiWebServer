d51c4bdb1527e2c98a785d13b60cd7bb03620def
nino <nino@nino.com>
2023-03-23 19:23:14 +0100

    Added database,changed design

--- WebServer.py
+++ WebServer.py
@@ -1,31 +1,62 @@
 import RPi.GPIO as GPIO
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+from database_handler import get_device_status, update_device_status, get_device_name, update_device_name, get_database_connection, create_table
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
 GPIO.setwarnings(False)
 
-# define actuators GPIOs
+# Define actuators GPIOs
 Relay = 40
 Sensor1 = 35
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(Relay, GPIO.OUT)
-GPIO.output(Relay, GPIO.LOW)
-#GPIO.setup(Sensor1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-#GPIO.setup(Sensor1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+#GPIO.output(Relay, GPIO.LOW)
 GPIO.setup(Sensor1, GPIO.IN)
+
+def init_device_data():
+    device_names = {
+        'relay': get_device_name('relay'),
+        'sensor1': get_device_name('sensor1'),
+    }
+    device_status = {
+        'relay': get_device_status('relay'),
+        'sensor1': get_device_status('sensor1'),
+    }
+    return device_names, device_status
+
+if __name__ == '__main__':
+    create_table()
+    device_names, device_status = init_device_data()
+
+GPIO.output(Relay, GPIO.HIGH if device_status['relay'] else GPIO.LOW)
+
 
 @app.route("/")
 def index():
+    return render_template('index.html', device_names=device_names, device_status=device_status)
+
+@socketio.on('update_device_name')
+def update_device_name_socketio(data):
+    device_key = data['device_key']
+    device_name = data['device_name']
+    device_names[device_key] = device_name
+    emit('device_name_updated', {'device_key': device_key, 'device_name': device_name}, broadcast=True)
+    update_device_name(device_key, device_name)
+
+
+@socketio.on('connect')
+def handle_connect():
     RelaySts = GPIO.input(Relay)
     Sensor1Sts = GPIO.input(Sensor1)
-    templateData = {
-        'Relay': RelaySts,
-        'Sensor1' : Sensor1Sts,
-    }
-    return render_template('index.html', **templateData)
+    emit('relay_status', {'Relay': RelaySts})
+    emit('sensor1_status', {'Sensor1': Sensor1Sts})
+
+    # Emit device names
+    for device_key, device_name in device_names.items():
+        emit('device_name_updated', {'device_key': device_key, 'device_name': device_name})    
 
 @socketio.on('relay_update')
 def handle_relay_update(data):
@@ -36,12 +67,15 @@ def handle_relay_update(data):
         GPIO.output(Relay, GPIO.LOW)
 
     RelaySts = GPIO.input(Relay)
+    update_device_status('relay', RelaySts)
     emit('relay_status', {'Relay': RelaySts}, broadcast=True)
 
 def sensor1_callback(channel):
     Sensor1Sts = GPIO.input(channel)
-    print("Sensor1Sts:", Sensor1Sts)  # Add this print statement
+    print("Sensor1Sts:", Sensor1Sts)
+    update_device_status('sensor1', Sensor1Sts)
     socketio.emit('sensor1_status', {'Sensor1': Sensor1Sts}, namespace='/')
+
 
 GPIO.add_event_detect(Sensor1, GPIO.BOTH, callback=sensor1_callback, bouncetime=50)
 
---
+++ database_handler.py
@@ -0,0 +1,68 @@
+import sqlite3
+from sqlite3 import Error
+
+def get_database_connection():
+    print("get_database_connection called")
+    conn = None
+    try:
+        conn = sqlite3.connect('devices2.db')
+    except Error as e:
+        print(e)
+    return conn
+
+def create_table():
+    conn = get_database_connection()
+    if conn is not None:
+        c = conn.cursor()
+        c.execute('''CREATE TABLE IF NOT EXISTS devices
+                     (device_key TEXT PRIMARY KEY, device_name TEXT, device_status INTEGER)''')
+        conn.commit()
+
+        # Insert default data if the table is empty
+        c.execute('SELECT COUNT(*) FROM devices')
+        count = c.fetchone()[0]
+        if count == 0:
+            c.execute('INSERT INTO devices (device_key, device_name, device_status) VALUES (?, ?, ?)', ('relay', 'Relay', 0))
+            c.execute('INSERT INTO devices (device_key, device_name, device_status) VALUES (?, ?, ?)', ('sensor1', 'Sensor 1', 0))
+            conn.commit()
+
+        conn.close()
+
+def get_device_status(device_key):
+    conn = get_database_connection()
+    if conn is not None:
+        c = conn.cursor()
+        c.execute('SELECT device_status FROM devices WHERE device_key=?', (device_key,))
+        status = c.fetchone()
+        conn.close()
+        return status[0] if status is not None else None
+    return None
+
+def update_device_status(device_key, device_status):
+    conn = get_database_connection()
+    if conn is not None:
+        c = conn.cursor()
+        c.execute('UPDATE devices SET device_status=? WHERE device_key=?', (device_status, device_key))
+        conn.commit()
+        conn.close()
+
+def get_device_name(device_key):
+    conn = get_database_connection()
+    if conn is not None:
+        c = conn.cursor()
+        c.execute('SELECT device_name FROM devices WHERE device_key=?', (device_key,))
+        name = c.fetchone()
+        conn.close()
+        print(f"Name for {device_key}: {name[0] if name is not None else None}")
+        return name[0] if name is not None else None
+    return None
+
+
+def update_device_name(device_key, device_name):
+    conn = get_database_connection()
+    if conn is not None:
+        c = conn.cursor()
+        c.execute('UPDATE devices SET device_name=? WHERE device_key=?', (device_name, device_key))
+        conn.commit()
+        conn.close()
+
