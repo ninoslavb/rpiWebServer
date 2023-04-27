f4529cf348643b3665e71a811ab26352be4d4940
nino <nino@nino.com>
2023-04-27 12:28:21 +0200

    Removed subscription to logging topic

--- zigbee.py
+++ zigbee.py
@@ -18,7 +18,7 @@ def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
     client.subscribe("zigbee2mqtt/bridge/event")
     client.subscribe("zigbee2mqtt/bridge/log")
-    client.subscribe("zigbee2mqtt/bridge/logging")
+    #client.subscribe("zigbee2mqtt/bridge/logging")
     client.subscribe("zigbee2mqtt/+")  # Subscribe to all device topics
 
 
